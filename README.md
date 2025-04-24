4b4afde238c1689e9330a88e8a2c81002c9d2067
ButterDebugger <34288129+ButterDebugger@users.noreply.github.com>
2025-04-23 22:42:10 -0400

    Added project files

---
+++ README.md
@@ -0,0 +1,17 @@
+# Vite Hono Bun Template
+
+This template should help get you started developing with [Vite](https://vite.dev/) and [Hono](https://hono.dev/) and [Bun](https://bun.sh/).
+
+## Commands
+
+To install dependencies:
+
+```bash
+bun install
+```
+
+To run the development server:
+
+```bash
+bun run dev
+```
---
+++ src/index.ts
@@ -0,0 +1,18 @@
+import { Hono } from "hono";
+import { logger } from "hono/logger";
+import { serveStatic } from "hono/bun";
+
+const port = process.env.PORT ?? 3000;
+const isDev = process.env.NODE_ENV === "development";
+const app = new Hono();
+
+// Enable logger in development
+if (isDev) app.use("*", logger());
+
+// Serve bundled files
+app.get("/*", serveStatic({ root: "dist" }));
+
+export default {
+	port,
+	fetch: app.fetch
+};
